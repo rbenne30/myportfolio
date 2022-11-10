e71dd9ed49ffd8b79e4131eac2d8cf2616c2edc9
rbenne30 <ryannebennett1@outlook.com>
2022-11-10 17:45:48 -0500

    added repo and website links to app

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -12,13 +12,15 @@ export default function Work() {
       </div>
       <div className="work-container" id="work">
 
-        {apps.map(({ key, title, img, description }) =>
+        {apps.map(({ key, title, img, description, repo, website }) =>
 
           <div className="application" key={key}>
             <img src={img} alt={title} />
             <div className="description">
               <h2>{title}</h2>
               <p>{description}</p>
+              <a href={repo} taget="_blank" className="githubBtn"><button>repo</button></a>
+              <a href={website} taget="_blank" className="websiteBtn"><button>website</button></a>
             </div>
           </div>
         )}
--- src/components/work/applications.js
+++ src/components/work/applications.js
@@ -1,37 +1,50 @@
 import img1 from './images/hidden-gems.png';
 import img2 from './images/README.png';
 import img3 from './images/scheduler.png';
+import img4 from './images/passwordgenerator.png';
+import img5 from './images/randomcat facts.png';
 
 const apps = [
   {
     key: 1,
-    title: "Web App",
+    title: "Travel App",
     img: `${img1}`,
-    description: "A discription of the app"
+    description: " Helps the user find exotic and rare places to travel ",
+    repo:"https://github.com/rbenne30/Hidden-Gems.git",
+    website: "https://thawing-springs-82201.herokuapp.com/",
+    
   },
   {
     key: 2,
     title: "README APP",
     img: `${img2}`,
-    description: "A discription of the app"
+    description: "A discription of the app",
+    repo: "https://github.com/rbenne30/Team-Profile-Generator-.git",
+    website: "https://youtu.be/KHyesLJnPHQ",
   },
   {
     key: 3,
     title: "Workday Scheduler",
     img: `${img3}`,
-    description: "A discription of the app"
+    description: "A planner to help organize your work schedule",
+    repo:"https://github.com/rbenne30/Work-Day-",
+    website:"https://rbenne30.github.io/Work-Day-/",
   },
   {
     key: 4,
-    title: "App 4",
-    img: "https://picsum.photos/200/200",
-    description: "A discription of the app"
+    title: "Security APP",
+    img: `${img4}`,
+    description: "Help user generate a secure password",
+    repo:"https://github.com/rbenne30/robot-gladiators-",
+    website:"https://rbenne30.github.io/Password-Generator/",
   },
   {
     key: 5,
-    title: "App 5",
-    img: "https://picsum.photos/200/200",
-    description: "A discription of the app"
+    title: " CSS APP",
+    img: `${img5}`,
+    description: "Demo of CSS",
+    repo: "https://github.com/Obtains/Cat-Facts-and-Pictures/tree/main",
+    website:"https://obtains.github.io/Cat-Facts-and-Pictures/",
   }
 ]
 
